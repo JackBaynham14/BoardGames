b7ec9e99f6c3fd1d4ca21e02281cf1dad761d1ae
JackBaynham14 <jack_baynham@outlook.com>
2021-01-02 10:54:29 -0700

    Initialize Main Loop

---
+++ Tic Tac Toe/TicTacToe.py
@@ -0,0 +1,11 @@
+'''
+This program simulates a game of TicTacToe. It interacts with the Board object
+    and additional modules such as graphics and networking.
+'''
+
+from Board import Board
+
+def main():
+    pass
+
+#main()
